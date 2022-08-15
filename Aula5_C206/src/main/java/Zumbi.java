bd84a8b292180a79946cc64871512cc63a7843b6
MazinhaB <mariana.barude@gec.inatel.br>
2022-08-15 18:20:32 -0300

    Zumbi

---
+++ Aula5_C206/src/main/java/Zumbi.java
@@ -0,0 +1,20 @@
+public class Zumbi {
+    String nome;
+    int pessoas_devoradas;
+    int pessoas_transformadas;
+    boolean fome;
+
+    void devorar(){
+        pessoas_devoradas++;
+        fome = false;
+        System.out.println("HUAGMAHRJAHAHHARDSA");
+    }
+    void transformar(){
+        pessoas_transformadas++;
+        fome = true;
+        System.out.println("HUAGMAHRJAHAHHARDSA");
+    }
+    void falar(){
+        System.out.println("HUAGMAHRJAHAHHARDSA");
+    }
+}
